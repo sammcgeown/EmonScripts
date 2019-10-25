413b15253627792ee0d3f5b4803cca1a456bcba8
sammcgeown <sam@mcgeown.co.uk>
2019-10-25 13:55:12 +0100

    Create init-docker.sh

---
+++ install/init-docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+user=$USER
+openenergymonitor_dir=/opt/openenergymonitor
+emoncms_dir=/opt/emoncms
+
+sudo apt-get update -y
+sudo apt-get install -y git-core
+
+sudo mkdir -p $openenergymonitor_dir
+sudo chown $user $openenergymonitor_dir
+
+sudo mkdir -p $emoncms_dir
+sudo chown $user $emoncms_dir
+
+cd $openenergymonitor_dir
+
+git clone https://github.com/openenergymonitor/EmonScripts.git
+cd $openenergymonitor_dir/EmonScripts
+git checkout stable
+
+cd $openenergymonitor_dir/EmonScripts/install
+./main.sh
+cd
+
+rm init.sh
